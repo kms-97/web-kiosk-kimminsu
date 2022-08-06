311a11799a2c85bbe96c4d1041f9d439d9a8b348
kms-97 <msg016@naver.com>
2022-08-07 01:24:17 +0900

    feat: food 모듈 추가
    - food Dto 작성
    - food Controller 작성
      get /api/foods 경로의 요청에 대해 쿼리에 categoryId가 있다면 해당 카테고리 아이디를 가진 food들을
      아니라면 모든 food에 대한 정보를 반환
    - food service 작성
      현재 날짜로부터 이전 7일간의 주문 건수를 조회하여 해당 아이템의 주문 비율을 계산,
      0.1 이상이라면 인기를 true로 설정하여 dto를 반환

---
+++ server/src/food/food.service.ts
@@ -0,0 +1,54 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { FoodDto } from 'src/dto/food.dto';
+import { Food } from 'src/entity/food.entity';
+import { Between, DataSource } from 'typeorm';
+import { getToday, getWeekAgo } from 'src/util/time';
+import { OrderItem } from 'src/entity/orderItem.entity';
+
+@Injectable()
+export class FoodService {
+  constructor(@Inject('DATA_SOURCE') private dataSource: DataSource) {}
+
+  async getAllFoods(): Promise<FoodDto[]> {
+    const today = getToday();
+    const startDate = getWeekAgo(today);
+
+    const foodEntities = await this.dataSource
+      .createQueryBuilder()
+      .select('food')
+      .from(Food, 'food')
+      .leftJoinAndSelect('food.orders', 'orderItem')
+      .getMany();
+
+    const orderItemEntities = await this.dataSource.manager.find(OrderItem, {
+      where: {
+        createdAt: Between(startDate.toString(), today.toString()),
+      },
+    });
+
+    const totalOrderInWeek = orderItemEntities.reduce((total, item) => total + item.unit, 0);
+    return foodEntities.map((food) => FoodDto.fromFoodEntity(food, totalOrderInWeek));
+  }
+
+  async getFoodByCategoryId(categoryId: number): Promise<FoodDto[]> {
+    const today = getToday();
+    const startDate = getWeekAgo(today);
+
+    const foodEntities = await this.dataSource
+      .createQueryBuilder()
+      .select('food')
+      .from(Food, 'food')
+      .leftJoinAndSelect('food.orders', 'orderItem')
+      .where('food.category_id = :id', { id: categoryId })
+      .getMany();
+
+    const orderItemEntities = await this.dataSource.manager.find(OrderItem, {
+      where: {
+        createdAt: Between(startDate.toString(), today.toString()),
+      },
+    });
+
+    const totalOrderInWeek = orderItemEntities.reduce((total, item) => total + item.unit, 0);
+    return foodEntities.map((food) => FoodDto.fromFoodEntity(food, totalOrderInWeek));
+  }
+}
