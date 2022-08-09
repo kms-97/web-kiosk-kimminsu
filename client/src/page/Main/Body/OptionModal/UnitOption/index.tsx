139d02addbee07a4295cfeb24188da00d0a86e26
kms-97 <msg016@naver.com>
2022-08-09 17:01:20 +0900

    feat: 수량 조절 부분 추가

---
+++ client/src/page/Main/Body/OptionModal/UnitOption/index.tsx
@@ -0,0 +1,31 @@
+import { FlexContainer, CircleButton } from '../../../../../component';
+import { click } from '../../../../../util/pointerEvent';
+import styles from './UnitOption.module.scss';
+
+interface props {
+  unit: number;
+  increaseUnit: () => void;
+  decreaseUnit: () => void;
+}
+
+const UnitOption = ({ unit, increaseUnit, decreaseUnit }: props) => {
+  const onClickIncreaseButton = click(10, increaseUnit);
+  const onClickDecreaseButton = click(10, decreaseUnit);
+  return (
+    <FlexContainer flow="row" wrap="nowrap" gap="10px">
+      <CircleButton
+        isActive={!(unit === 1)}
+        onPointerDown={onClickDecreaseButton}
+        className={styles.button}
+      >
+        <div>-</div>
+      </CircleButton>
+      <div className={styles.unit}>{unit}</div>
+      <CircleButton isActive={true} onPointerDown={onClickIncreaseButton} className={styles.button}>
+        <div>+</div>
+      </CircleButton>
+    </FlexContainer>
+  );
+};
+
+export default UnitOption;
