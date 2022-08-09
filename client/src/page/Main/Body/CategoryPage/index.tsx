27843f116e72df540855b548a3b81e8bebd9cd33
kms-97 <msg016@naver.com>
2022-08-09 17:03:39 +0900

    feat: 모달 창 구현
    - 상품 선택 아직 미구현

--- client/src/page/Main/Body/CategoryPage/index.tsx
+++ client/src/page/Main/Body/CategoryPage/index.tsx
@@ -1,17 +1,23 @@
 import { useState } from 'react';
-import { FlexContainer } from '../../../../component';
+import { FlexContainer, ModalContainer } from '../../../../component';
+import { click } from '../../../../util/pointerEvent';
 import FoodItem from '../FoodItem';
+import OptionModal from '../OptionModal';
 import styles from './CategoryPage.module.scss';
 
 interface props {
   foods: FOOD[];
+  getOptions: (arg: number) => { size: SIZE; temperature: TEMPERATURE };
 }
 
-const CategoryPage = ({ foods }: props) => {
+const CategoryPage = ({ foods, getOptions }: props) => {
   const [selectedItem, setSelectedItem] = useState<FOOD>();
+
   const selectItem = (foodId: number) => {
     setSelectedItem(foods.flat().filter(({ id }) => id === foodId)[0]);
   };
+
+  const closeModal = click(10, setSelectedItem, undefined, true);
 
   return (
     <>
@@ -20,6 +26,14 @@ const CategoryPage = ({ foods }: props) => {
           <FoodItem food={food} onClick={selectItem} key={food.id} />
         ))}
       </FlexContainer>
+      {selectedItem ? (
+        <ModalContainer onPointerDown={closeModal}>
+          <OptionModal food={selectedItem} getOptions={getOptions} />
+        </ModalContainer>
+      ) : (
+        ''
+      )}
+    </>
   );
 };
 
---
+++ client/src/page/Main/Body/OptionModal/index.tsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import { FlexContainer, Img } from '../../../../component';
+import SizeOption from './SizeOption';
+import TemperatureOption from './TemperatureOption';
+import styles from './OptionModal.module.scss';
+import UnitOption from './UnitOption';
+
+interface props {
+  food: FOOD;
+  getOptions: (arg: number) => { size: SIZE; temperature: TEMPERATURE };
+}
+
+const OptionModal = ({ food, getOptions }: props) => {
+  const [eachPrice, setEachPrice] = useState<number>(0);
+  const [unit, setUnit] = useState<number>(1);
+  const { size, temperature } = getOptions(food.id);
+
+  useEffect(() => {
+    setEachPrice(Number(food.basePrice));
+  }, [food]);
+
+  const changeEachPrice = (beforePrice: number, afterPrice: number) => {
+    setEachPrice((prev) => prev - beforePrice + afterPrice);
+  };
+
+  const increaseUnit = () => {
+    setUnit((prev) => prev + 1);
+  };
+
+  const decreaseUnit = () => {
+    setUnit((prev) => prev - 1);
+  };
+
+  return (
+    <div className={styles.modal}>
+      <FlexContainer flow="column" wrap="nowrap" gap="10px">
+        <FlexContainer flow="row" wrap="nowrap" justify="spaceAround">
+          <Img src={food.imgURL} description={food.name} className={styles.img} />
+          <FlexContainer flow="column" wrap="nowrap" gap="10px" className={styles.name}>
+            <div>{food.name}</div>
+            <div>{eachPrice * unit} 원</div>
+          </FlexContainer>
+        </FlexContainer>
+        <div>수량</div>
+        <UnitOption unit={unit} increaseUnit={increaseUnit} decreaseUnit={decreaseUnit} />
+        <div>크기</div>
+        <SizeOption size={size} changeEachPrice={changeEachPrice} />
+        <div>온도</div>
+        <TemperatureOption temperature={temperature} changeEachPrice={changeEachPrice} />
+      </FlexContainer>
+    </div>
+  );
+};
+
+export default OptionModal;
