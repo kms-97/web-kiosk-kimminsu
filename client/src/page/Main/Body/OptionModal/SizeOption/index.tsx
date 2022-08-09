92e8b2b89aa04f5d43096a06141ec14ac5938a9d
kms-97 <msg016@naver.com>
2022-08-09 17:02:08 +0900

    feat: 사이즈 선택 부분 추가

---
+++ client/src/page/Main/Body/OptionModal/SizeOption/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { FlexContainer, OptionButton } from '../../../../../component';
+import { click } from '../../../../../util/pointerEvent';
+import styles from './SizeOption.module.scss';
+
+interface props {
+  size: SIZE;
+  changeEachPrice: (arg1: number, arg2: number) => void;
+}
+
+const SizeOption = ({ size, changeEachPrice }: props) => {
+  const [selectedSize, setSelectedSize] = useState<string>('small');
+
+  useEffect(() => {
+    setSelectedSize('small');
+  }, [size]);
+
+  const Button = ({ name, addPrice }: { name: string; addPrice: number | null }) => {
+    const selectSize = () => {
+      changeEachPrice(getPrice(selectedSize), getPrice(name));
+      setSelectedSize(name);
+    };
+
+    const getPrice = (name: string) => {
+      return Number(size[name]) as number;
+    };
+
+    const onPointerDown = click(10, selectSize);
+
+    return (
+      <OptionButton
+        isActive={addPrice === null ? false : true}
+        isSelected={name === selectedSize}
+        onPointerDown={onPointerDown}
+        className={styles.button}
+      >
+        <div>{name}</div>
+        <div>{addPrice}</div>
+      </OptionButton>
+    );
+  };
+
+  return (
+    <FlexContainer flow="row" wrap="nowrap" justify="spaceBetween">
+      {Object.entries(size).map(([name, addPrice]) => {
+        if (name !== 'id') {
+          return <Button key={name} name={name} addPrice={addPrice} />;
+        } else {
+          return <React.Fragment key={name}></React.Fragment>;
+        }
+      })}
+    </FlexContainer>
+  );
+};
+
+export default SizeOption;
