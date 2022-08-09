0a75a8917bda7e8fcd37d299dedec9dea3f59e0d
kms-97 <msg016@naver.com>
2022-08-09 17:01:44 +0900

    feat: 온도 선택 부분 추가

---
+++ client/src/page/Main/Body/OptionModal/TemperatureOption/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { FlexContainer, OptionButton } from '../../../../../component';
+import { click } from '../../../../../util/pointerEvent';
+import styles from './TemperatureOption.module.scss';
+
+interface props {
+  temperature: TEMPERATURE;
+  changeEachPrice: (arg1: number, arg2: number) => void;
+}
+
+const TemperatureOption = ({ temperature, changeEachPrice }: props) => {
+  const [selectedTemperature, setSelectedTemperature] = useState<string>('hot');
+
+  useEffect(() => {
+    setSelectedTemperature('hot');
+  }, [temperature]);
+
+  const Button = ({ name, addPrice }: { name: string; addPrice: number | null }) => {
+    const selectTemperature = () => {
+      changeEachPrice(getPrice(selectedTemperature), getPrice(name));
+      setSelectedTemperature(name);
+    };
+
+    const getPrice = (name: string) => {
+      return Number(temperature[name]) as number;
+    };
+
+    const onPointerDown = click(10, selectTemperature);
+
+    return (
+      <OptionButton
+        isActive={addPrice === null ? false : true}
+        isSelected={name === selectedTemperature}
+        onPointerDown={onPointerDown}
+        className={styles.button}
+      >
+        <div>{name}</div>
+        <div>{addPrice}</div>
+      </OptionButton>
+    );
+  };
+
+  return (
+    <FlexContainer flow="row" wrap="nowrap" justify="spaceBetween">
+      {Object.entries(temperature).map(([name, addPrice]) => {
+        if (name !== 'id') {
+          return <Button key={name} name={name} addPrice={addPrice} />;
+        } else {
+          return <React.Fragment key={name}></React.Fragment>;
+        }
+      })}
+    </FlexContainer>
+  );
+};
+
+export default TemperatureOption;
