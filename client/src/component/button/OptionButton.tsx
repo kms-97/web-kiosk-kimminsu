7933639eb340f10510b8d5d7841a88fb73efa1ca
kms-97 <msg016@naver.com>
2022-08-09 16:58:31 +0900

    refactor: 버튼이 내용이 아닌 children을 받도록 수정

--- client/src/component/button/OptionButton.tsx
+++ client/src/component/button/OptionButton.tsx
@@ -3,21 +3,19 @@ import styles from './OptionButton.module.scss';
 interface props extends React.ComponentProps<'button'> {
   isActive: boolean;
   isSelected: boolean;
-  name: string;
-  price: number | null;
 }
 
-const OptionButton = ({ isActive, isSelected, name, price, onClick, className }: props) => {
+const OptionButton = ({ isActive, isSelected, onPointerDown, className, children }: props) => {
   const classString = `
     ${styles.button}
     ${isSelected ? styles.selected : ''}
+    ${isActive ? '' : styles.disabled}
     ${className ?? ''}
   `;
 
   return (
-    <button className={classString} disabled={!isActive} onClick={onClick}>
-      <div>{name}</div>
-      <div>{price} 원</div>
+    <button className={classString} disabled={!isActive} onPointerDown={onPointerDown}>
+      {children}
     </button>
   );
 };
