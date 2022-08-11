3bb8fcacea9cc49caeca679068348e91ff3c7ead
kms-97 <msg016@naver.com>
2022-08-12 01:30:55 +0900

    chore: db 패스워드 설정 추가

--- server/src/database/database.providers.ts
+++ server/src/database/database.providers.ts
@@ -10,6 +10,7 @@ export const databaseProviders = [
         host: process.env.DATABASE_HOST,
         port: process.env.DATABASE_PORT,
         username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD ?? '',
         database: process.env.DATABASE,
         entities: [...entities],
         synchronize: true,
