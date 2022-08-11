f506b5941f75115db7c35e83f8a0acd58f66a267
kms-97 <msg016@naver.com>
2022-08-11 10:13:48 +0900

    feat: 커버 페이지 추가

---
+++ client/src/page/CoverPage/index.tsx
@@ -0,0 +1,34 @@
+import { useContext, useEffect } from 'react';
+import { FlexContainer, OutlineButton } from '../../component';
+import { OrderContext, PageContext } from '../../context';
+import { click } from '../../util/pointerEvent';
+import MainPage from '../MainPage';
+import styles from './CoverPage.module.scss';
+
+const CoverPage = () => {
+  const page = useContext(PageContext);
+  const order = useContext(OrderContext);
+
+  useEffect(() => {
+    order?.action.setState([]);
+  }, []);
+
+  const moveToCategoryPage = () => {
+    page?.action.addPage(MainPage);
+  };
+
+  const startOrder = click({ callback: moveToCategoryPage });
+
+  return (
+    <div className="page">
+      <FlexContainer flow="column" className={styles.cover} justifyContent="spaceAround">
+        <div className={styles.title}>KIOSK</div>
+        <OutlineButton className={styles.button} onPointerDown={startOrder}>
+          주문하기
+        </OutlineButton>
+      </FlexContainer>
+    </div>
+  );
+};
+
+export default CoverPage;
