18a4f24020052820a2efa0d080c18b1489e2fa2b
kms-97 <msg016@naver.com>
2022-08-11 21:21:52 +0900

    chore: 타이틀, 파비콘, 설명 등 수정

--- client/src/page/LoadingPage/index.tsx
+++ client/src/page/LoadingPage/index.tsx
@@ -1,4 +1,3 @@
-import indicator from 'asset/indicator.gif';
 import { FlexContainer, Img } from 'component';
 import styles from './LoadingPage.module.scss';
 
@@ -6,7 +5,11 @@ const LoadingPage = ({ msg }: { msg: string }) => {
   return (
     <div className="page">
       <FlexContainer className={styles.loading} flow="column" gap="30px">
-        <Img src={indicator} description="loading" className={styles.logo} />
+        <Img
+          src="https://user-images.githubusercontent.com/72490858/184129637-7aa93a82-c98c-4a72-898f-6b3ec86e0518.gif"
+          description="loading"
+          className={styles.logo}
+        />
         <div>{msg}</div>
       </FlexContainer>
     </div>
