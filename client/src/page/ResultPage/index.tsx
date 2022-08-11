92ffbe36f0ddf8bb54c61b2c0d57218e1d5231ff
kms-97 <msg016@naver.com>
2022-08-12 05:26:03 +0900

    feat: ui 개선

--- client/src/page/ResultPage/index.tsx
+++ client/src/page/ResultPage/index.tsx
@@ -1,5 +1,5 @@
 import { useContext, useRef, useEffect } from 'react';
-import { FlexContainer } from 'component';
+import { DragContainer, FlexContainer } from 'component';
 import { PAYMENT_STRING, SIZE_STRING, TEMP_STRING } from 'constant';
 import { OrderContext } from 'context';
 import styles from './ResultPage.module.scss';
@@ -45,30 +45,32 @@ const ResultPage = ({ setPage, payment, orderNum, credit }: props) => {
       <FlexContainer flow="column" gap="20px" justifyContent="start" className={styles.result}>
         <div className={styles.title}>주문 확인</div>
         <FlexContainer className={styles.receipt}>
-          <FlexContainer flow="column" className={styles.content} alignItems="start" gap="20px">
-            <div className={styles.number}>주문 번호 {orderNum}</div>
-            <div>주문 상세</div>
-            <FlexContainer flow="column" alignItems="start" gap="10px">
-              {orders?.state.map((order) => (
-                <div>
-                  {order.name}, {TEMP_STRING[order.temperature]}, {SIZE_STRING[order.size]},{' '}
-                  {order.unit}개
-                </div>
-              ))}
+          <DragContainer direction="y" className={styles.container}>
+            <FlexContainer flow="column" className={styles.content} alignItems="start" gap="20px">
+              <div className={styles.number}>주문 번호 {orderNum}</div>
+              <div>주문 상세</div>
+              <FlexContainer flow="column" alignItems="start" gap="10px">
+                {orders?.state.map((order) => (
+                  <div>
+                    {order.name}, {TEMP_STRING[order.temperature]}, {SIZE_STRING[order.size]},{' '}
+                    {order.unit}개
+                  </div>
+                ))}
+              </FlexContainer>
+              <FlexContainer flow="column" alignItems="start" gap="10px">
+                <div>결제 방식: {PAYMENT_STRING[payment]}</div>
+                <div>결제 금액: {totalPrice.toLocaleString()}원</div>
+                {payment === 'cash' ? (
+                  <>
+                    <div>투입 금액: {credit.toLocaleString()}원</div>
+                    <div>반환 금액: {(credit - totalPrice).toLocaleString()}원</div>
+                  </>
+                ) : (
+                  ''
+                )}
+              </FlexContainer>
             </FlexContainer>
-            <FlexContainer flow="column" alignItems="start" gap="10px">
-              <div>결제 방식: {PAYMENT_STRING[payment]}</div>
-              <div>결제 금액: {totalPrice.toLocaleString()}원</div>
-              {payment === 'cash' ? (
-                <>
-                  <div>투입 금액: {credit.toLocaleString()}원</div>
-                  <div>반환 금액: {(credit - totalPrice).toLocaleString()}원</div>
-                </>
-              ) : (
-                ''
-              )}
-            </FlexContainer>
-          </FlexContainer>
+          </DragContainer>
         </FlexContainer>
         <div>
           이 화면은 <span ref={countRef}></span>초 뒤 사라집니다.
